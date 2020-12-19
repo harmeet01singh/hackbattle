bf14113047766ecae9e15c3429b8895dcfefa06b
RohanpolGIT <rohanpol36@gmail.com>
2020-12-19 23:02:31 +0530

    sign in and log in updated

--- client/src/components/LogInComponent.jsx
+++ client/src/components/LogInComponent.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function LogInComponent() {
     return (
-        <div>
-            Here there be log in
+        <div className="container p-5 ">
+            <form className="border border-secondary rounded bg-white ">
+            <h3 className="px-2 pt-3 pd-1 text-center">Create Your account by Signing In</h3>
+            <hr className="w-100"></hr>
+            {/*1*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Email</label>
+                <input type="email" className="form-control col-8" />
+            </div>
+            {/*2*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Password</label>
+                <input type="password" className="form-control col-8" />
+            </div>
+            {/*submit button*/}
+            <div className='row p-3 justify-content-center'>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            </form>
         </div>
     )
 }
--- client/src/components/SignInComponent.jsx
+++ client/src/components/SignInComponent.jsx
@@ -2,21 +2,62 @@ import React from 'react'
 
 export default function SignInComponent() {
     return (
-        <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div className="container p-5 ">
+            <form className="border border-secondary rounded bg-white ">
+            <h3 className="px-2 pt-3 pd-1 text-center">Create Your account by Signing In</h3>
+            <hr className="w-100"></hr>
+            {/*1*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Name</label>
+                <input type="text" className="form-control col-8" />
+            </div>
+            {/*2*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Email</label>
+                <input type="email" className="form-control col-8" />
+            </div>
+            {/*3*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Password</label>
+                <input type="password" className="form-control col-8" />
+            </div>
+            {/*4*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Gender</label>
+                <div class="form-check col-sm-4">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                    <label class="form-check-label" for="inlineRadio1">Male</label>
+                </div>
+                <div class="form-check col-sm-4">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                    <label class="form-check-label" for="inlineRadio2">Female</label>
+                </div>
+            </div>
+            {/*5*/}
+            <div class="row p-3 m-2">
+                <label for="" className="col-4">Upload Profile Image</label>
+                <input type="file" class="form-control-file col-8" />
+            </div>
+            {/*6*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Phone Number</label>
+                <input type="number" className="form-control col-8" />
+            </div>
+            {/*7*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Address</label>
+                <input type="text" className="form-control col-8" />
+            </div>
+            {/*8*/}
+            <div className='row p-3 m-2'>
+                <label for="" className='col-4'>Bio</label>
+                <textarea className="form-control col-8" rows="3"></textarea>
+            </div>
+            {/*submit button*/}
+            <div className='row p-3 justify-content-center'>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
     )
 }
